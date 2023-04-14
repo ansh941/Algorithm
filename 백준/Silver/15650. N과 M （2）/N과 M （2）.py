c8cda5a6dc38ec25ad119c773eef34a112edd74c
ansh941 <36464983+ansh941@users.noreply.github.com>
2023-04-14 15:09:59 +0900

    [Silver III] Title: N과 M (2), Time: 68 ms, Memory: 28776 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,24 @@
+N, M = map(int, input().split())
+nums = [i+1 for i in range(N)]
+
+def print_seq(seq):
+    for i in range(M):
+        print(seq[i], end=' ')
+    print()
+    
+# 현재 수열, 선택 가능 숫자
+def sequence(seq, n):
+    if len(seq)==M:
+        print_seq(seq)
+        return
+    
+    for i in range(len(n)):
+        if len(seq) > 0 and n[i] < seq[-1]:
+            continue
+        next_seq = seq.copy()
+        next_seq.append(n[i])
+        next_n = n.copy()
+        next_n.remove(n[i])
+        sequence(next_seq, next_n)
+        
+sequence([],nums)
