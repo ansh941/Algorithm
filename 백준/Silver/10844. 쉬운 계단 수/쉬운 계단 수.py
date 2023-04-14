81fd85604ce75d6fbfd128775919fa0bd4eee233
ansh941 <36464983+ansh941@users.noreply.github.com>
2023-04-14 15:06:55 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 76 ms, Memory: 28776 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -0,0 +1,20 @@
+N = int(input())
+
+dp = [[0 for _ in range(10)] for _ in range(N)]
+
+for i in range(1,10):
+    dp[0][i] = 1
+    
+'''
+현재 수에 따라 dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
+'''
+for i in range(1,N):
+    for j in range(10):
+        if j == 0:
+            dp[i][j] = dp[i-1][j+1]
+        elif j == 9:
+            dp[i][j] = dp[i-1][j-1]
+        else:
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
+        dp[i][j] %= 1000000000
+print(sum(dp[N-1])%1000000000)
